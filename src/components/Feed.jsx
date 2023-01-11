0b077ab521f99cb5e4e6c96e3c824d529aec5af0
Borys Mch <borys.mch@gmail.com>
2023-01-11 05:56:06 +0200

    Added Video grid and Video Card

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,19 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Sidebar, Videos } from "./";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [video, setVideo] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideo(data.items)
+    );
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -11,7 +22,10 @@ const Feed = () => {
           borderRight: "1px solid #3d3d3d",
           px: { sx: 0, md: 2 },
         }}>
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
 
         <Typography
           className="copyright"
@@ -19,6 +33,18 @@ const Feed = () => {
           sx={{ mt: 1.5, color: "fff" }}>
           Copyright 2022 JSM Media
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={video} />
       </Box>
     </Stack>
   );
